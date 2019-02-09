d863f9adbef29044b323c1630c81a71454ae3f44
Damir Shagaev <damir.vivelapaix@gmail.com>
2019-02-09 17:44:37 +0300

    fix - FileTreeModel constructor has been fixed

--- src/main/java/filemanager/models/FileTreeModel.java
+++ src/main/java/filemanager/models/FileTreeModel.java
@@ -4,7 +4,6 @@ import javax.swing.filechooser.FileSystemView;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeNode;
-import java.io.File;
 import java.util.Arrays;
 
 public class FileTreeModel extends DefaultTreeModel {
@@ -13,17 +12,10 @@ public class FileTreeModel extends DefaultTreeModel {
         super(root);
 
         FileSystemView fileSystemView = FileSystemView.getFileSystemView();
-        File[] roots = fileSystemView.getRoots();
 
-        for (File fileSystemRoot : roots) {
-            DefaultMutableTreeNode node = new DefaultMutableTreeNode(fileSystemRoot);
-            ((DefaultMutableTreeNode)root).add(node);
-
-            Arrays.stream(fileSystemView.getFiles(fileSystemRoot, true))
-                    .sorted((o1, o2) -> o1.getName()
-                            .compareToIgnoreCase(o2.getName()))
-                    .map(DefaultMutableTreeNode::new)
-                    .forEach(node::add);
-        }
+        Arrays.stream(fileSystemView.getRoots())
+                .sorted((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()))
+                .map(DefaultMutableTreeNode::new)
+                .forEach(((DefaultMutableTreeNode)root)::add);
     }
 }
